bc1113f236beb84338ffc49227443ad3d0ef27c7
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-11-22 09:49:31 +0100

    feat: add header partial

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container my-5">
 
-    <h1 class="my-3">HOME</h1>
+    <h1 class="my-3">Ticket Platform</h1>
     <h2 class="my-3">Pagina Pubblica</h2>
     <p class="my-3">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, culpa dignissimos atque commodi est velit aut quasi animi iure cumque dolor fuga rerum asperiores, eaque officiis. Dolore, quis quo. Accusamus placeat blanditiis atque perferendis velit similique beatae nam reiciendis consectetur amet quia quae iste cumque eveniet sed voluptate, et eius esse. Quae voluptas odio fugit quam ducimus, accusantium, consectetur commodi maiores repellendus earum, alias sequi nesciunt minus omnis quidem. Debitis, quis blanditiis culpa placeat ut numquam pariatur ipsam illum atque animi rerum in praesentium alias, necessitatibus ipsa explicabo similique amet architecto ratione! Ducimus perferendis esse cupiditate nemo, possimus harum exercitationem.
